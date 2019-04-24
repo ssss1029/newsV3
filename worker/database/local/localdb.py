c323d0f0e755149b163bd2d71e90897c4733ad8a
Saurav Kadavath <sauravkadavath@berkeley.edu>
2019-04-24 13:25:22 -0700

    rewrite localdatabase

--- worker/database/local/localdb.py
+++ worker/database/local/localdb.py
@@ -3,9 +3,59 @@ This file houses a local db object. This allows us to develop without having an 
 instance running. This is only for testing purposes
 
 Note that all data storage is in memory, so do not run on this mode for too long.
+Also note that this is news application specific, and hence contains articles and sources
 """
 import logging
 
+from tinydb import TinyDB, Query
+from ..dbobject import DBObject
+from ...utils.general import copy_dict_shallow
+
 class LocalDatabase(object):
-    def __init__(self):
-        pass
+    """
+    Based on tinydb.
+    Currently only supports articles and sources
+    """
+
+    def __init__(self, articles=None, sources=None):
+
+        self.db = TinyDB('database.json')
+        self.articles = db.table('articles')
+        self.sources = db.table('sources')
+
+        for idx, article in enumerate(self.articles):
+            self.articles.insert(copy_dict_shallow(article))
+
+        for idx, source in enumerate(self.sources):
+            self.sources.insert(copy_dict_shallow(source))
+
+        self.tables = { 'articles', 'sources' }
+
+
+    def save_object(obj_type, obj):
+        """
+        Save the object to the database
+        """
+        assert isinstance(obj, DBObject)
+        assert obj_type in self.tables
+
+        table = getattr(self, obj_type)
+        table.insert(obj.get_save_data())
+
+
+    def get(obj_type, query):
+        assert obj_type in self.tables
+        table = getattr(self, obj_type)
+
+        ret = list()
+        for obj in table:
+            is_match = True
+            for key, value in query:
+                if obj.get(key, None) != value:
+                    is_match = False
+                    break
+
+            if is_match:
+                ret.append(copy_dict_shallow(obj))
+
+        return ret
--- worker/utils/general.py
+++ worker/utils/general.py
@@ -1,3 +1,5 @@
+
+import collections
 import logging
 
 logger = logging.getLogger(__name__)
@@ -5,3 +7,18 @@ logger = logging.getLogger(__name__)
 def testLogger(string):
     logger.debug(string)
 
+def copy_dict_shallow(d):
+    """
+    Make a shallow copy of the given dict, while checking if all keys and values are hashable
+    """
+    result = dict()
+    for key, value in d:
+        key_hashable = isinstance(key, collections.Hashable)
+        value_hashable = isinstance(value, collections.Hashable)
+
+        if not key_hashable or not value_hashable:
+            raise ValueError("{0} : {1} is not hashable".format(key, value))
+        else:
+            result[key] = value
+
+    return result
