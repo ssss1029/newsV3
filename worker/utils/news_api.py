4c70f81c6182c1f7fbfa916f964cacb06c70bbff
Saurav Kadavath <sauravkadavath@berkeley.edu>
2019-04-24 11:09:42 -0700

    Make dedicated util for newsapi.org

---
+++ worker/utils/news_api.py
@@ -0,0 +1,89 @@
+"""
+This file hold functions and constants that are specific to the
+News API at http://newsapi.org
+"""
+
+news_api_categories = {
+    "business",
+    "entertainment",
+    "general",
+    "health",
+    "science",
+    "sports",
+    "technology"
+}
+
+news_api_languages = {
+    "ar",
+    "de",
+    "en"
+    "es",
+    "fr",
+    "he",
+    "it",
+    "nl",
+    "no",
+    "pt",
+    "ru",
+    "se",
+    "ud",
+    "zh"
+}
+
+news_api_countries = {
+    "ae",
+	"ar",
+	"at",
+	"au",
+	"be",
+	"bg",
+	"br",
+	"ca",
+	"ch",
+	"cn",
+	"co",
+	"cu",
+	"cz",
+	"de",
+	"eg",
+	"fr",
+	"gb",
+	"gr",
+	"hk",
+	"hu",
+	"id",
+	"ie",
+	"il",
+	"in",
+	"it",
+	"jp",
+	"kr",
+	"lt",
+	"lv",
+	"ma",
+	"mx",
+	"my",
+	"ng",
+	"nl",
+	"no",
+	"nz",
+	"ph",
+	"pl",
+	"pt",
+	"ro",
+	"rs",
+	"ru",
+	"sa",
+	"se",
+	"sg",
+	"si",
+	"sk",
+	"th",
+	"tr",
+	"tw",
+	"ua",
+	"us",
+	"ve",
+	"za
+}
+
--- worker/utils/source.py
+++ worker/utils/source.py
@@ -2,102 +2,21 @@
 A class that will house a news source. (e.g. CNN, BBC, etc.)
 """
 
-news_api_categories = {
-    "business",
-    "entertainment",
-    "general",
-    "health",
-    "science",
-    "sports",
-    "technology"
-}
-
-news_api_languages = {
-    "ar",
-    "de",
-    "en"
-    "es",
-    "fr",
-    "he",
-    "it",
-    "nl",
-    "no",
-    "pt",
-    "ru",
-    "se",
-    "ud",
-    "zh"
-}
-
-news_api_countries = {
-    "ae",
-	"ar",
-	"at",
-	"au",
-	"be",
-	"bg",
-	"br",
-	"ca",
-	"ch",
-	"cn",
-	"co",
-	"cu",
-	"cz",
-	"de",
-	"eg",
-	"fr",
-	"gb",
-	"gr",
-	"hk",
-	"hu",
-	"id",
-	"ie",
-	"il",
-	"in",
-	"it",
-	"jp",
-	"kr",
-	"lt",
-	"lv",
-	"ma",
-	"mx",
-	"my",
-	"ng",
-	"nl",
-	"no",
-	"nz",
-	"ph",
-	"pl",
-	"pt",
-	"ro",
-	"rs",
-	"ru",
-	"sa",
-	"se",
-	"sg",
-	"si",
-	"sk",
-	"th",
-	"tr",
-	"tw",
-	"ua",
-	"us",
-	"ve",
-	"za
-}
+from news_api import news_api_categories, news_api_languages, news_api_countries
 
 class NewsSource(object):
+	"""
+	Hold a news source object.
+	This assumes that the object is from the News API
+	"""
     def __init__(self, _id, name, description, url, category, language, country):
-        # Do sanity checks
-        category_check = category in news_api_categories
-        lang_check     = language in news_api_languages
-        country_check  = country in news_api_countries
 
-        if not category_check:
+        # Do sanity checks
+        if category not in news_api_categories:
             raise ValueError("Unrecognized category: {0}".format(category))
-        if not lang_check:
+        if language not in news_api_languages:
             raise ValueError("Unrecognized language: {0}".format(language))
-        if not country_check:
+        if language not in news_api_languages:
             raise ValueError("Unrecognized country: {0}".format(country))
 
         self.id = _id
@@ -113,4 +32,3 @@ class NewsSource(object):
         Return a readble representaion of self
         """
         return "<NewsSource {0}: {0}>".format(self.id, self.name)
-
