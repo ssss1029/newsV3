6310c90ac05d6ac4f18619d29e23ba642fa7159f
Saurav Kadavath <sauravkadavath@gmail.com>
2019-05-09 00:43:43 -0700

    FINALLY installed boilerpipe

--- news-worker/worker/utils/general.py
+++ news-worker/worker/utils/general.py
@@ -22,3 +22,21 @@ def copy_dict_shallow(d):
             result[key] = value
 
     return result
+
+def copy_dict_deep(d):
+    """
+    Recursively make a deep copy of the given dictionary. All keys and values must be hashable
+    """
+    result = dict()
+    for key, value in d.items():
+        key_hashable = isinstance(key, collections.Hashable)
+        value_hashable = isinstance(value, collections.Hashable)
+
+        if isinstance(value, dict) and key_hashable:
+            result[key] = copy_dict_deep(d[key])
+        elif key_hashable and value_hashable:
+            result[key] = value
+        else:
+            raise NotImplementedError("{0} : {1} is not implemented".format(key, value ))
+    
+    return result
